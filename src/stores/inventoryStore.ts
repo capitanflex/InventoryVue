71f242e8f81615c2831175f55af8b585152365b7
capitanflex <danilov.zo@yandex.ru>
2024-08-01 21:59:19 +0300

    add localStorage for inventory store

--- src/stores/inventoryStore.ts
+++ src/stores/inventoryStore.ts
@@ -1,9 +1,10 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import type ICell from '@/interfaces/ICell.ts'
 
-export const useInventoryStore = defineStore('inventory', () => {
-    const cells = ref<ICell[]>([
+const getInitialCells = (): ICell[] => {
+    const storedCells = localStorage.getItem('cells');
+    return storedCells ? JSON.parse(storedCells) : [
         { id: 1, item: 'green', count: 3 },
         { id: 2, item: 'yellow', count: 1 },
         { id: 3, item: 'purple', count: 2 },
@@ -29,10 +30,13 @@ export const useInventoryStore = defineStore('inventory', () => {
         { id: 23, item: null, count: 0 },
         { id: 24, item: null, count: 0 },
         { id: 25, item: null, count: 0 }
-    ])
+    ];
+}
 
-    const updateCell = (oldCellId : Number, newCell : ICell) => {
+export const useInventoryStore = defineStore('inventory', () => {
+    const cells = ref<ICell[]>(getInitialCells());
 
+    const updateCell = (oldCellId: number, newCell: ICell) => {
         const oldCell = cells.value.find(c => c.id === oldCellId);
         if (oldCell) {
             const temp = [newCell.item, newCell.count];
@@ -41,19 +45,33 @@ export const useInventoryStore = defineStore('inventory', () => {
             oldCell.item = temp[0];
             oldCell.count = temp[1];
         }
+        saveStateToLocalStorage();
     }
 
     const updateCellCount = (cellInfo: ICell, countToDelete: number) => {
         const cell = cells.value.find(c => c.id === cellInfo.id);
         if (!cell) return;
-        if (cell.count - countToDelete == 0) {
+
+        if (cell.count - countToDelete === 0) {
             cell.count = 0;
             cell.item = null;
-        }
-        else {
+        } else {
             cell.count -= countToDelete;
         }
+        saveStateToLocalStorage();
     }
 
+    const saveStateToLocalStorage = () => {
+        localStorage.setItem('cells', JSON.stringify(cells.value));
+    }
+
+    watch(
+        () => cells.value,
+        (newCells) => {
+            localStorage.setItem('cells', JSON.stringify(newCells));
+        },
+        { deep: true }
+    );
+
     return { cells, updateCellCount, updateCell }
-})
+});
